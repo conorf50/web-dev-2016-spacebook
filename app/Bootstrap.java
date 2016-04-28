e81c2f94f815b298e6f3a6a2a2456965706dad25
conorf50 <conorf50@gmail.com>
2016-04-28 19:04:43 +0100

    Final commit
    .yaml file loaded with everything but blog posts + comments

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,19 +1,28 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 import play.*;
+import play.db.jpa.Blob;
 import play.jobs.*;
+import play.libs.MimeTypes;
 import play.test.*;
  
 import models.*;
  
 @OnApplicationStart
-public class Bootstrap extends Job 
-{ 
-  public void doJob()
-  {
-    if (User.count() == 0)
-    {
-      Fixtures.loadModels("data.yml");
-    }
-  }
+public class Bootstrap extends Job
+{
+ public void doJob() throws FileNotFoundException
+ {
+ Fixtures.deleteDatabase();
+ Fixtures.loadModels("data.yml");
+
+ String photoName = "public/images/homer.gif";
+ Blob blob = new Blob ();
+ blob.set(new FileInputStream(photoName), MimeTypes.getContentType(photoName));
+ User homer = User.findByEmail("homer@simpson.com");
+ homer.profilePicture = blob;
+ homer.save();
+ }
 }
